c6b8709d9c2c8eba97ead7c4867ba8e717dc8e52
Lea ANDRE <aleaandre@gmail.com>
2024-11-29 12:13:14 +0100

    [AF-113] : handler DTO response for 200 and 401 status, with JSON format

--- rentalapi/src/main/java/com/rentalapp/rentalapi/controller/LoginController.java
+++ rentalapi/src/main/java/com/rentalapp/rentalapi/controller/LoginController.java
@@ -1,15 +1,20 @@
 package com.rentalapp.rentalapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rentalapp.rentalapi.dto.ErrorResponse;
 import com.rentalapp.rentalapi.dto.LoginRequest;
+import com.rentalapp.rentalapi.dto.LoginResponse;
 import com.rentalapp.rentalapi.service.JWTService;
 
 @RestController
@@ -23,9 +28,8 @@ public class LoginController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
-        System.out.println(loginRequest.getLogin());
-        System.out.println(loginRequest.getPassword());
+    @ResponseBody
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -33,11 +37,12 @@ public class LoginController {
                             loginRequest.getPassword()));
             System.out.println("Authentication successful");
 
-            return jwtService.generateToken(authentication);
+            return ResponseEntity.ok(new LoginResponse(jwtService.generateToken(authentication)));
+
         } catch (Exception e) {
             System.err.println("Authentication failed: " + e.getClass().getName() + " - " + e.getMessage());
             e.printStackTrace();
-            throw new RuntimeException("Invalid credentials", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
         }
     }
 
