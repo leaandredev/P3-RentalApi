926963f9a29b06722039bc20144fbe3f30ac3f43
Lea ANDRE <aleaandre@gmail.com>
2024-12-03 12:08:07 +0100

    [AF-158] : refactor Auth controller and service

---
+++ rentalapi/src/main/java/com/rentalapp/rentalapi/service/AuthService.java
@@ -0,0 +1,53 @@
+package com.rentalapp.rentalapi.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.rentalapp.rentalapi.dto.RegisterRequest;
+import com.rentalapp.rentalapi.dto.TokenResponse;
+import com.rentalapp.rentalapi.dto.UserResponse;
+import com.rentalapp.rentalapi.model.DbUser;
+import com.rentalapp.rentalapi.repository.UserRepository;
+
+@Service
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JWTService jwtService;
+
+    private final UserService userService;
+
+    private final UserRepository userRepository;
+
+    AuthService(AuthenticationManager authenticationManager, JWTService jwtService, UserService userService,
+            UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    public TokenResponse authentificate(String email, String password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        password));
+        System.out.println("Authentication successful");
+
+        return new TokenResponse(jwtService.generateToken(authentication));
+    }
+
+    public TokenResponse login(RegisterRequest registerRequest) {
+        DbUser user = userService.createUser(registerRequest);
+        return this.authentificate(user.getEmail(), registerRequest.getPassword());
+    }
+
+    public UserResponse getAuthenticatedUser(String email) {
+        DbUser dbUser = userRepository.findByEmail(email);
+        return new UserResponse(dbUser);
+    }
+}
