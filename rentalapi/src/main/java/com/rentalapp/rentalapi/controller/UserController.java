b602ec8aa9a8a3197f9d03bc9298271fe34d1d1f
Lea ANDRE <aleaandre@gmail.com>
2024-12-05 14:44:15 +0100

    [AF-176] : annotation in controller

--- rentalapi/src/main/java/com/rentalapp/rentalapi/controller/UserController.java
+++ rentalapi/src/main/java/com/rentalapp/rentalapi/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.rentalapp.rentalapi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rentalapp.rentalapi.dto.UserResponse;
 import com.rentalapp.rentalapi.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +26,11 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(description = "Récupération des informations de propriétaire")
+    @ApiResponse(responseCode = "200", description = "Propriétaire récupéré", content = @Content(schema = @Schema(implementation = UserResponse.class)))
+    @ApiResponse(responseCode = "401", description = "Connexion non autorisé")
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
         return ResponseEntity.ok(userService.getUserById(Integer.valueOf(id)));
     }
